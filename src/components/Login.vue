d301425280c88f2eda47a1a1dfe1c83407ee0b6e
renlishi <renlishi@360jk.com>
2019-10-31 15:22:03 +0800

    登录页面

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -25,8 +25,8 @@
       login () {
         this.$axios
           .post('/login', {
-            username: this.loginForm.username,
-            password: this.loginForm.password
+            signInName: this.loginForm.username,
+            passWord: this.loginForm.password
           })
           .then(successResponse => {
             if (successResponse.data.code === 200) {
