bd8ef4ea7baee8350e30eb5e0b4e727d203c1b63
renlishi <renlishi@360jk.com>
2019-11-05 11:29:21 +0800

    页面显示

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,44 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// 导入刚才编写的组件
-import AppIndex from '@/components/home/AppIndex'
-import Login from '@/components/Login'
+import AppIndex from '../components/home/AppIndex'
+import Login from '../components/Login'
+import Home from '../components/Home'
+import LibraryIndex from '../components/library/LibraryIndex'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',// Vue 中配置的路由默认是 hash 模式 这里修改为  history
+  mode: 'history',
   routes: [
-    // 下面都是固定的写法
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home,
+      redirect: '/index',
+      children: [
+        {
+          path: '/index',
+          name: 'AppIndex',
+          component: AppIndex,
+          meta: {
+            requireAuth: true
+          }
+        },
+        {
+          path: '/library',
+          name: 'Library',
+          component: LibraryIndex,
+          meta: {
+            requireAuth: true
+          }
+        }
+      ]
+    },
     {
       path: '/login',
       name: 'Login',
       component: Login
-    },
-    {
-      path: '/index',
-      name: 'AppIndex',
-      component: AppIndex,
-      meta: {
-        requireAuth: true
-      }
     }
   ]
 })
+
